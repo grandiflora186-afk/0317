a9f98f2d2cd5ad3db7bed62ca2558ed898efdc1b
grandiflora186-afk <grandiflora186@gmail.com>
2026-03-31 11:40:59 +0900

    main.py

--- 0331/main.py
+++ 0331/main.py
@@ -1,9 +1,29 @@
 from time import sleep
 from machine import Pin
-#create LED object from pin2,Set Pin2 to output
-led=Pin(2,Pin.OUT)
+
+led=Pin(5,Pin.OUT)
+led2=Pin(9,Pin.OUT)
+
+
+# 입력함수를 사용해서 '1' : led 켜지고, '0' 일 때 led 꺼지도록 설정
+from time import sleep
+from machine import Pin
+
+led=Pin(5,Pin.OUT)
+led2=Pin(9,Pin.OUT)
+
 while True:
-    led.value(0) #Set led turn on
-    sleep(1)
-    led.value(1) #Set led turn off
-    sleep(1)
+    n = input()
+    print(n)
+    if n == '1':
+        print("LED ON")
+        led.value(1) #Set led turn on
+        sleep(1)
+    else:
+        led.value(0) #Set led turn off
+        
+# while True:
+#     led.value(0) #Set led turn on
+#     sleep(1)
+#     led.value(1) #Set led turn off
+#     sleep(1)
